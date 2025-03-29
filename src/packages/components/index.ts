e87935fdf5db663fdae5288a6015a6e5c6a44f7d
span_grid <gridctrlitsolutions@gmail.com>
2025-03-29 17:43:24 +0530

    Result summary component

--- src/packages/components/index.ts
+++ src/packages/components/index.ts
@@ -1,3 +1,4 @@
 export { Button } from "./button/button"
 export { ProductCard } from "./product-card/product-card"
 export { Rating } from './rating/rating'
+export { ResultsSummary } from './results-summary/results-summary'
